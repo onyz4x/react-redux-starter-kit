462ff603883a2d3f69eeb5f32ab5a938e873c6c4
lx <181621732@qq.com>
2016-08-27 12:33:48 +0800

    add material ui

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import Header from '../../components/Header'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import classes from './CoreLayout.scss'
 // import 'antd/dist/antd.css'
  import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
+  <MuiThemeProvider>
   <div className='container text-center'>
     <Header />
     <div className={classes.mainContainer}>
       {children}
     </div>
   </div>
+  </MuiThemeProvider>
 )
 
 CoreLayout.propTypes = {
--- src/routes/GridDemo/components/GridDemo.js
+++ src/routes/GridDemo/components/GridDemo.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { DatePicker ,Menu,Breadcrumb} from 'antd';
 import { IndexLink, Link } from 'react-router'
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
 export const GridDemo = (props) => (
   <div> 
 
-   
+     <AppBar
+    title="Title"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
   </div>
 )
 
